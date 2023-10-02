506916948dce970513e49feb63a70093dee18aab
JoKurth <71062996+JoKurth@users.noreply.github.com>
2023-10-02 14:32:16 +0200

    implemented a method to calculate double(phi)

---
+++ src/PresburgerMapper/DoublePhi.hs
@@ -0,0 +1,21 @@
+module PresburgerMapper.DoublePhi (
+    doublePredicate
+) where
+
+import Helper.List (flatten)
+import qualified Types.Predicates as Predicates (RemainderPredicate(..), ThresholdPredicate(..), BasePredicate(..), Predicate(..))
+
+double :: [[Int]] -> [[Int]]
+-- double = map (flatten . map (\x -> [[x], [2*x]])) -- see Definition 2 (ist der Link von 7.6.1 zu Definition 2 richtig?)
+double [] = []
+double [[]] = [[]]
+double (x:xs) = [[head x], [2 * head x]] ++ double xs
+
+doubleBasePredicate :: Predicates.BasePredicate -> Predicates.BasePredicate
+doubleBasePredicate (Left (Predicates.RP sum m c)) = Left (Predicates.RP (double sum) m c)
+doubleBasePredicate (Right (Predicates.TP sum m)) = Right (Predicates.TP (double sum) m)
+
+doublePredicate :: Predicates.Predicate -> Predicates.Predicate
+doublePredicate (Predicates.NodeP lPred operator rPred) = Predicates.NodeP (doublePredicate lPred) operator (doublePredicate rPred)
+doublePredicate (Predicates.NodeN operator pred) = Predicates.NodeN operator (doublePredicate pred)
+doublePredicate (Predicates.Leaf pred) = Predicates.Leaf (doubleBasePredicate pred)

ee3c45d3b003610f2ec618b7f0cf015b95e7c9d4
JoKurth <71062996+JoKurth@users.noreply.github.com>
2023-10-02 14:51:34 +0200

    implemented preprocessing step

---
+++ src/Transformer/Preprocess.hs
@@ -0,0 +1,21 @@
+module Transformer.Preprocess (
+    preprocess
+) where
+
+import qualified Types.PopulationComputer as PC (PopulationComputer(..))
+
+import qualified Data.Set as Set
+import qualified Data.MultiSet as MultiSet
+
+preprocess :: PC.PopulationComputer String -> PC.PopulationComputer String
+preprocess pc = PC.PC {
+    PC.states = Set.unions [PC.states pc, Set.fromList ["h"], Set.fromList xStars],
+    PC.delta = Set.union (PC.delta pc) (Set.fromList starTransitions),
+    PC.input = Set.fromList xStars,
+    -- PC.output = 
+    PC.helpers = MultiSet.union (PC.helpers pc) (MultiSet.insert "h" MultiSet.empty)
+}
+    where
+        xAndxStars = map (\x -> (x, x ++ "_*")) $ Set.toList $ PC.input pc
+        xStars = map snd xAndxStars
+        starTransitions = map (\tuple -> (MultiSet.fromList [snd tuple, "h"], MultiSet.fromList [fst tuple, "h"])) xAndxStars

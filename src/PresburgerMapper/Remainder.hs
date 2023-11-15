f0a461924df05c0c6b5df4e16557af8149f2e427
JoKurth <71062996+JoKurth@users.noreply.github.com>
2023-11-15 17:41:03 +0100

    fixed a bug that unary transitions were generated in certain edge cases

--- src/PresburgerMapper/Remainder.hs
+++ src/PresburgerMapper/Remainder.hs
@@ -28,7 +28,13 @@ constructRemainderPC (Predicates.RP coefficients m c) = PC.PCB {
         stateList = 0 : [2^i | i <- [0 .. d]]
         transitions =    [(MultiSet.fromList [2^i, 2^i], MultiSet.fromList [2^(i+1), 0]) | i <- [0 .. d-1]] -- combine
                       -- TODO: clean up the following line
-                      ++ [(MultiSet.fromList $ flatten [[2^d], map (0 *) [1 .. len-1]], MultiSet.fromList $ map (2^) bin) | let bin = intToBinary (2^d - m), let len = length bin] -- modulo
+                      ++ if 2^d - m == 0
+                            then [(MultiSet.fromList [2^d, 0], MultiSet.fromList [0, 0])]
+                            else [(MultiSet.fromList $ flatten [[2^d], map (0 *) [1 .. len-1]], MultiSet.fromList $ map (2^) bin) |
+                                    let bin = intToBinary (2^d - m),
+                                    let len = length bin] -- modulo
                       -- TODO: try if \_ -> can be replaced with const
-                      ++ [(MultiSet.fromList $ map (\_ -> 2^d) [1 .. d], MultiSet.fromList $ map (2^) bin ++ map (0*) [1 .. d-len]) | let bin = intToBinary (d * 2^d `mod` m), let len = length bin] -- fast modulo
+                      ++ [(MultiSet.fromList $ map (\_ -> 2^d) [1 .. d], MultiSet.fromList $ map (2^) bin ++ map (0*) [1 .. d-len]) |
+                                let bin = intToBinary (d * 2^d `mod` m),
+                                let len = length bin] -- fast modulo
         outputFunction = buildRemainderCircuit m (c `mod` m) d
